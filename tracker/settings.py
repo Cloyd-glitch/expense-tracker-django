09c050277afa8f01fa5c77d70fe6f6eb1b95e8fd
Cloyd Arvie G. Floremonte <s.cagfloremonte@usm.edu.ph>
2026-09-12 03:07:12 +0800

    fix: use CompressedStaticFilesStorage to reliably serve CSS on Railway

--- tracker/settings.py
+++ tracker/settings.py
@@ -136,7 +136,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'  # Where collectstatic puts files for production
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 
 # Security settings (active in production only)
