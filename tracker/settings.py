b641ca0e223b137eb79a7a83cf2377010bd492bd
Cloyd Arvie G. Floremonte <s.cagfloremonte@usm.edu.ph>
2026-09-12 03:14:26 +0800

    fix: STATIC_URL must start with / for absolute CSS paths across all pages

--- tracker/settings.py
+++ tracker/settings.py
@@ -134,7 +134,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'  # Where collectstatic puts files for production
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
