a9761a113284e8e7ad8010151a84611447e9aa87
Cloyd Arvie G. Floremonte <s.cagfloremonte@usm.edu.ph>
2026-09-12 02:54:34 +0800

    fix: bind gunicorn to Railway PORT and allow railway.app hosts

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn tracker.wsgi --log-file -
+web: gunicorn tracker.wsgi --bind 0.0.0.0:$PORT --log-file -
--- tracker/settings.py
+++ tracker/settings.py
@@ -34,6 +34,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-local-dev-fallback-on
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
+# Allow all Railway subdomains as a fallback
+ALLOWED_HOSTS += ['.railway.app', '.up.railway.app']
 
 
 # Application definition
